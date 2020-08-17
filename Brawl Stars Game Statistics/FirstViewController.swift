59b9c18b2fe1e3918617bdf54a3c850742567d80
Jeremy Hou <jeremyrhou@gmail.com>
2020-08-17 19:02:53 -0400

    Initial Commit

---
+++ Brawl Stars Game Statistics/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Brawl Stars Game Statistics
+//
+//  Created by Jeremy on 8/17/20.
+//  Copyright © 2020 Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Brawl Stars Game Statistics/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Brawl Stars Game Statistics
+//
+//  Created by Jeremy on 8/17/20.
+//  Copyright © 2020 Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
